33f3e6af9ec28a37ff6010d46e6a1e92ee489f98
islem gharsallah <islemgharsallah86@gmail.com>
2025-03-10 09:44:14 +0100

    third commit

---
+++ src/Entity/Livre.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LivreRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LivreRepository::class)]
+class Livre
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $ISBN = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Slug = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Image = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Resume = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Editeur = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $DateEdition = null;
+
+    #[ORM\Column]
+    private ?float $Prix = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getISBN(): ?string
+    {
+        return $this->ISBN;
+    }
+
+    public function setISBN(string $ISBN): static
+    {
+        $this->ISBN = $ISBN;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->Slug;
+    }
+
+    public function setSlug(string $Slug): static
+    {
+        $this->Slug = $Slug;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->Image;
+    }
+
+    public function setImage(string $Image): static
+    {
+        $this->Image = $Image;
+
+        return $this;
+    }
+
+    public function getResume(): ?string
+    {
+        return $this->Resume;
+    }
+
+    public function setResume(string $Resume): static
+    {
+        $this->Resume = $Resume;
+
+        return $this;
+    }
+
+    public function getEditeur(): ?string
+    {
+        return $this->Editeur;
+    }
+
+    public function setEditeur(string $Editeur): static
+    {
+        $this->Editeur = $Editeur;
+
+        return $this;
+    }
+
+    public function getDateEdition(): ?\DateTimeInterface
+    {
+        return $this->DateEdition;
+    }
+
+    public function setDateEdition(\DateTimeInterface $DateEdition): static
+    {
+        $this->DateEdition = $DateEdition;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->Prix;
+    }
+
+    public function setPrix(float $Prix): static
+    {
+        $this->Prix = $Prix;
+
+        return $this;
+    }
+}
